8d0091eb28a4af4dd3dbb2f333e69cde1fe66013
MaryTiiaa <marina.tschirky@thread-it.ch>
2026-05-23 09:13:55 +0200

    Extract GameSession into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,13 @@
-import { useState } from 'react'; 
-import './App.css'; 
-import Button from './Button'; 
- 
-const question = { 
-  text: "Welcher Begriff kommt im ICAO-Buchstabieralphabet zuerst?", 
-  answers: ["Alfa", "Bravo", "Charlie"], 
-  correctAnswer: "Alfa" 
-}; 
- 
-function App() { 
-  const [feedback, setFeedback] = useState(null); 
-  const [score, setScore] = useState(0); 
-   const [isAnswered, setIsAnswered] = useState(false);
-  const handleAnswerClick = (selectedAnswer) => { 
-  setIsAnswered(true); 
- 
-  if (selectedAnswer === question.correctAnswer) { 
-    setFeedback("Richtig!"); 
-    setScore(score + 1); 
-  } else { 
-    setFeedback(`Falsch! Die richtige Antwort wäre: ${question.correctAnswer}`); 
-  } 
-};
+import './App.css';
+import GameSession from './GameSession';
 
-  return ( 
-  <div> 
-    <h1>Willkommen beim WISS-Quiz!</h1> 
-    <p>Punkte: {score}</p> 
-    <h2>{question.text}</h2> 
- 
-    <div> 
-      {question.answers.map((answer) => (
-        <Button 
-          key={answer} 
-          text={answer} 
-          onClick={() => handleAnswerClick(answer)} 
-          disabled={isAnswered} 
-        />
-      ))} 
-    </div> 
- 
-    {feedback && <p>{feedback}</p>} 
-  </div> 
-); 
-} 
- 
-export default App; 
+function App() {
+  return (
+    <div>
+      <h1>Willkommen beim WISS-Quiz!</h1>
+      <GameSession />
+    </div>
+  );
+}
+
+export default App;
